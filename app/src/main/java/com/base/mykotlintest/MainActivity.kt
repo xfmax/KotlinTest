4efe93381e0fb80e3d0f7647052edee6771be765
sufeng <sufeng@keep.com>
2020-09-18 13:03:25 +0800

    a

--- app/src/main/java/com/base/mykotlintest/MainActivity.kt
+++ app/src/main/java/com/base/mykotlintest/MainActivity.kt
@@ -7,38 +7,39 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        countDownController.enterAnimation();
-        var currentTimer = 20.0
-        var poisition = 20.0
-        var totalDuration = 10.0
-        buttonStart.setOnClickListener {
-            countDownController.type = CountDownController.TYPE_TIME;
-//            countDownController.startTimerSchedule(30.0,100.0,100 / 100.toDouble())
-            Thread {
-                while (true){
-                    Thread.sleep(200)
-                    countDownController.updateStepProgress(totalDuration,currentTimer,poisition,totalDuration)
-                    currentTimer += 0.2
-                }
-            }.start()
+        val a = 0
+//        setContentView(R.layout.activity_main)
+//        countDownController.enterAnimation();
+//        var currentTimer = 20.0
+//        var poisition = 20.0
+//        var totalDuration = 10.0
+//        buttonStart.setOnClickListener {
+//            countDownController.type = CountDownController.TYPE_TIME;
+////            countDownController.startTimerSchedule(30.0,100.0,100 / 100.toDouble())
+//            Thread {
+//                while (true){
+//                    Thread.sleep(200)
+//                    countDownController.updateStepProgress(totalDuration,currentTimer,poisition,totalDuration)
+//                    currentTimer += 0.2
+//                }
+//            }.start()
 
         }
 
-        buttonEnd.setOnClickListener {
-            if (buttonEnd.text == "END") {
-                countDownController.pause()
-                buttonEnd.text = "RESUME"
-            }else{
-                countDownController.resume()
-                buttonEnd.text = "END"
-            }
-        }
-
-
-        buttonReset.setOnClickListener {
-            countDownController.reset()
-        }
-    }
+//        buttonEnd.setOnClickListener {
+//            if (buttonEnd.text == "END") {
+//                countDownController.pause()
+//                buttonEnd.text = "RESUME"
+//            }else{
+//                countDownController.resume()
+//                buttonEnd.text = "END"
+//            }
+//        }
+//
+//
+//        buttonReset.setOnClickListener {
+//            countDownController.reset()
+//        }
+//    }
 
 }
